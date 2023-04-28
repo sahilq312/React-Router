4a0602d55ef97818fff71cd458d90e88f03dc8c1
Sahil <quraishisahil04@outlook.com>
2023-04-28 12:32:25 +0530

    relative routes

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="index" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="vans" element={<Vans />} />
         <Route path="vans/:id" element={<VanDetail />} />
